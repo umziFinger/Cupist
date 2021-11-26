d101b148b7fcfcce7b1dd5500b9c76bab6fe41e3
20wook93 <20wook93@gmail.com>
2021-11-26 18:45:23 +0900

    [BOLIMI-35] 약관 및 정책 상세 스크린 공통화 작업 [BOLIMI-67] 홈 하단 사업자 정보 영역 작업

---
+++ src/Containers/Common/PermissionDetailScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from '@/Components/Header';
+
+const PermissionDetailScreen = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Header type={'text'} text={'sdfsdf'} />
+    </View>
+  );
+};
+
+export default PermissionDetailScreen;
