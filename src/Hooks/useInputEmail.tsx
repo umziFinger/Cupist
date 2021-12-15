2f2d95f6c07a85b8a49b97a35d0a59daa1aee13f
hyunjin <>
2021-12-15 14:27:20 +0900

    [BOLIMI-153] 이메일 주소 형식 올바르지 않은데 문구가 표시되지 않음 예) ella@naver.con / ella@naver.comh 등..!

--- src/Hooks/useInputEmail.tsx
+++ src/Hooks/useInputEmail.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import AuthActions from '@/Stores/Auth/Actions';
-import { AuthState } from '@/Stores/Auth/InitialState';
 
 type ResultUseInputEmail = {
   email: string;
@@ -16,18 +13,62 @@ function useInputEmail(): ResultUseInputEmail {
   const [isEmailValid, setIsEmailValid] = useState(false);
   const [emailValidText, setEmailValidText] = useState('');
 
+  const handlerEmailType = (value: string) => {
+    const temp = value?.split('@');
+    if (/naver$/.test(temp[1]) || temp[1]?.split('.')[0] === 'naver') {
+      return 'naver';
+    }
+    if (/gmail$/.test(temp[1]) || temp[1]?.split('.')[0] === 'gmail') {
+      return 'gmail';
+    }
+    return null;
+  };
   // change
   const onChangeEmail = (value: string) => {
     if (value) {
-      const emailRegExp = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
-      if (value.match(emailRegExp)) {
-        setEmail(value);
-        setEmailValidText('');
-        setIsEmailValid(true);
-      } else {
-        setEmail(value);
-        setEmailValidText('이메일 형식이 올바르지 않습니다.');
-        setIsEmailValid(false);
+      const emailRegExp = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,3}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+      const naverEmailRegExp = /^[\w-]+(\.[\w-]+)*@naver.com$/;
+      const gmailEmailRegExp = /^[\w-]+(\.[\w-]+)*@gmail.com$/;
+
+      switch (handlerEmailType(value)) {
+        case 'naver': {
+          if (value.match(naverEmailRegExp)) {
+            setEmail(value);
+            setEmailValidText('');
+            setIsEmailValid(true);
+          } else {
+            setEmail(value);
+            setEmailValidText('네이버 이메일 형식이 올바르지 않습니다.');
+            setIsEmailValid(false);
+          }
+          break;
+        }
+
+        case 'gmail': {
+          if (value.match(gmailEmailRegExp)) {
+            setEmail(value);
+            setEmailValidText('');
+            setIsEmailValid(true);
+          } else {
+            setEmail(value);
+            setEmailValidText('Gmail 형식이 올바르지 않습니다.');
+            setIsEmailValid(false);
+          }
+          break;
+        }
+
+        default: {
+          if (value.match(emailRegExp)) {
+            setEmail(value);
+            setEmailValidText('');
+            setIsEmailValid(true);
+          } else {
+            setEmail(value);
+            setEmailValidText('이메일 형식이 올바르지 않습니다.');
+            setIsEmailValid(false);
+          }
+          break;
+        }
       }
     } else {
       setEmail('');
