3f212b2dfb0e9baddd8f96f1b36f935d3f33cd7c
Blue <blue@duople.com>
2022-05-27 12:21:15 +0900

    유의사항 디자인 수정

--- src/Containers/Reservation/ReservationScreen/WarningArea.tsx
+++ src/Containers/Reservation/ReservationScreen/WarningArea.tsx
@@ -1,29 +1,82 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import CustomText from '@/Components/CustomText';
 import { Color } from '@/Assets/Color';
 import { PlaceState } from '@/Stores/Place/InitialState';
 import Config from '@/Config';
+import CustomDashed from '@/Components/CustomDashed';
 
 const WarningArea = () => {
   const { selectedTicket } = useSelector((state: PlaceState) => state.place);
+  const warningItem = selectedTicket?.caution
+    ? selectedTicket?.caution
+    : selectedTicket?.eventType === 'normal'
+    ? Config.NORMAL_CAUTION_INFO
+    : Config.FREE_CAUTION_INFO;
+  console.log(warningItem?.split('\n'));
 
   return (
     <View style={{ flex: 1 }}>
       <View style={{ justifyContent: 'center' }}>
-        <CustomText style={{ color: Color.Black1000, fontSize: 15, fontWeight: '500', letterSpacing: -0.2 }}>
+        <CustomText style={{ color: Color.Error, fontSize: 15, fontWeight: '500', letterSpacing: -0.2 }}>
           유의 사항
         </CustomText>
       </View>
-      <View style={{ justifyContent: 'center', marginTop: 8 }}>
-        <CustomText style={{ color: Color.Gray600, fontSize: 12 }}>
-          {selectedTicket?.caution
-            ? selectedTicket?.caution
-            : selectedTicket?.eventType === 'normal'
-            ? Config.NORMAL_CAUTION_INFO
-            : Config.FREE_CAUTION_INFO}
-        </CustomText>
+      <View style={{ justifyContent: 'center', marginTop: 20 }}>
+        <FlatList
+          data={warningItem?.split('\n')}
+          renderItem={({ item, index }) => (
+            <View style={{ marginTop: index === 0 ? 0 : 8 }}>
+              <CustomText
+                style={{
+                  fontWeight: index === 0 ? '500' : 'normal',
+                  fontSize: index === 0 ? 13 : 12,
+                  letterSpacing: -0.2,
+                  lineHeight: 18,
+                  color: index === 0 ? 'black' : Color.Gray600,
+                }}
+              >
+                {item}
+              </CustomText>
+            </View>
+          )}
+          listKey={'title'}
+          keyExtractor={(item, index) => index.toString()}
+          initialNumToRender={4}
+          maxToRenderPerBatch={3}
+          windowSize={7}
+        />
+
+        {/* <CustomText style={{ fontWeight: '500', fontSize: 13, letterSpacing: -0.2, lineHeight: 18 }}> */}
+        {/*  {warningItem} */}
+        {/* </CustomText> */}
+        {/* <CustomText style={{ color: Color.Gray600, fontSize: 12, marginTop: 8 }}>{warningItemSubTitle}</CustomText> */}
+        <View style={{ height: 1, marginVertical: 20 }}>
+          <CustomDashed dashLength={2.7} dashColor={Color.Gray350} style={{ height: '100%' }} />
+        </View>
+
+        <FlatList
+          data={Config.COMMON_CAUTION_INFO?.split('\n') || []}
+          renderItem={({ item, index }) => (
+            <View style={{ flexDirection: 'row', marginTop: index === 0 ? 0 : 8 }}>
+              <View>
+                <CustomText style={{ color: Color.Gray600, fontSize: 12 }}>{'\u2022 '}</CustomText>
+              </View>
+              <View style={{ flex: 1 }}>
+                <CustomText style={{ color: Color.Gray600, fontSize: 12 }}>{item}</CustomText>
+              </View>
+            </View>
+          )}
+          listKey={'contents'}
+          keyExtractor={(item, index) => index.toString()}
+          initialNumToRender={2}
+          maxToRenderPerBatch={1}
+          windowSize={7}
+        />
+        {/* <View> */}
+        {/*  <CustomText style={{ color: Color.Gray600, fontSize: 12 }}>{warningItemContents}</CustomText> */}
+        {/* </View> */}
       </View>
     </View>
   );
