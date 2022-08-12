e107bab2e78c1bbbb8a5e833eafd0cec0fe84fae
Blue <blue@duople.com>
2022-08-12 18:40:15 +0900

    알바몬 배너 교체 및 유튜브 연동

--- src/Containers/Home/HomeScreen/AlbamonBanner.tsx
+++ src/Containers/Home/HomeScreen/AlbamonBanner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useWindowDimensions, View } from 'react-native';
+import { Linking, useWindowDimensions, View } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import moment from 'moment';
 import CustomButton from '@/Components/CustomButton';
@@ -12,13 +12,16 @@ const AlbamonBanner = () => {
     <View style={{ flex: 1, marginTop: 17, alignItems: 'center' }}>
       <CustomButton
         style={{ height: ((width - 40) / 336) * 186, width: width - 40 }}
-        onPress={() => navigate('AlbamonDetailScreen')}
+        onPress={() => {
+          Linking.openURL('vnd.youtube://channel/UCDB9dSAZh5jDE2adRfHbrrw').catch((e) => {
+            console.log(e);
+            Linking.openURL('https://www.youtube.com/channel/UCDB9dSAZh5jDE2adRfHbrrw');
+          });
+        }}
       >
         <FastImage
           style={{ width: '100%', height: '100%', borderRadius: 5 }}
-          source={{
-            uri: `https://s3.ap-northeast-2.amazonaws.com/cdn.bolimi.kr/bolimi/static/event/albamon/homeBanner%403x.png`,
-          }}
+          source={require('@/Assets/Images/Albamon/v31.png')}
           resizeMode={FastImage.resizeMode.cover}
         />
       </CustomButton>
